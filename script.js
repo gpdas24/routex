12cdd5d163290eedbd55d3225e93342ec7eca3e0
gpdas24 <164848329+gpdas24@users.noreply.github.com>
2024-08-26 23:14:05 +1400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Toggle the navigation menu on small screens
 document.getElementById('menuToggle').addEventListener('click', function() {
-    const navLinks = document.querySelector('.navbar');
+    const navLinks = document.querySelector('.navbar-menu');
     navLinks.classList.toggle('active');
 });
